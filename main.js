4a445186e43d20d23a5917a02da848161622f5a0
Gabriel Morales <gabrielmorales@Gabriels-MacBook-Pro.local>
2021-08-15 19:39:44 -0400

    change function declaration

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function statement(invoice, plays) {
             minimumFractionDigits: 2
         }).format;
 
-    function amountFor(play, aPerformance) {
+    function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -26,7 +26,7 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
@@ -37,7 +37,7 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         let thisAmount = 0;
-        thisAmount = amountFor(playFor(perf), perf);
+        thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
